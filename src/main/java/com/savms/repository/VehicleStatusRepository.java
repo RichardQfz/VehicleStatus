880b38c947b93924054d911498effedd2f71b0ab
RichardQfz <richardcheng2001@outlook.com>
2025-07-31 12:40:05 +1000

    Repository, service and controller. Only getVehicleSpeedByPlate method for now

--- src/main/java/com/savms/repository/VehicleStatusRepository.java
+++ src/main/java/com/savms/repository/VehicleStatusRepository.java
@@ -1,6 +1,22 @@
 package com.savms.repository;
 
+import com.savms.entity.VehicleStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class VehicleStatusRepository
 {
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
+    public double getVehicleSpeedByPlate( String plate )
+    {
+        Query query = new Query( Criteria.where( "plate" ).is( plate ) );
+        VehicleStatus vehicleStatus = mongoTemplate.findOne( query, VehicleStatus.class );
+        return vehicleStatus != null ? vehicleStatus.getSpeed() : -1;
+    }
 }
--- src/main/java/com/savms/service/VehicleStatusService.java
+++ src/main/java/com/savms/service/VehicleStatusService.java
@@ -1,5 +1,17 @@
 package com.savms.service;
 
+import com.savms.repository.VehicleStatusRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class VehicleStatusService
 {
+    @Autowired
+    private VehicleStatusRepository vehicleStatusRepository;
+
+    public double getSpeedByPlate( String plate )
+    {
+        return vehicleStatusRepository.getVehicleSpeedByPlate( plate );
+    }
 }
